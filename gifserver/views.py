05d6965608319af41ab3e9ee62653fc71e0b8d3f
Mike Dory <mike@dory.me>
2012-12-28 18:56:05 +0530

    adding some logic to the view

--- gifserver/views.py
+++ gifserver/views.py
@@ -1,13 +1,15 @@
 # Create your views here.
 from django.shortcuts import render_to_response
-from gifserver.models import Gifsite
+from gifserver.models import Gifsite, Gif
 
 
 def home(request):
-    gif_list = Gifsite.objects.all().order_by('-date_aded')[:5]
+    gif_sites = Gifsite.objects.all().order_by('-date_added')[:5]
+    gifs = Gif.objects.all().order_by('-date_added')[:5]
     return render_to_response(
         'gifserver/home.html',
         {
-            'gif_list': gif_list
+            'gifsites': gif_sites,
+            'gifs': gifs
         }
     )
