f23eedb564ac0d106820ab12205802c15fd29da1
Mike Dory <mike@dory.me>
2012-12-28 18:36:07 +0530

    hey look a view

--- gifserver/views.py
+++ gifserver/views.py
@@ -1,1 +1,13 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from gifserver.models import Gifsite
+
+
+def home(request):
+    gif_list = Gifsite.objects.all().order_by('-date_aded')[:5]
+    return render_to_response(
+        'gifserver/home.html',
+        {
+            'gif_list': gif_list
+        }
+    )
