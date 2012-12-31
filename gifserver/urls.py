4e4548e8b67bde05fbdba7b8eaa6add4bc6413fd
Mike Dory <mike@dory.me>
2012-12-31 10:43:03 +0530

    adding a url handler for the gifserver on its own

---
+++ gifserver/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+
+# urls for the gifserver app
+urlpatterns = patterns('gifserver.views',
+    url(r'^$', 'home', name='home'),
+    url(r'^gifsite/(?P<gifsite_name>\w+)/$', 'gifsite'),
+    url(r'^gif/(?P<gif_id>\d+)/$', 'gif'),
+)
