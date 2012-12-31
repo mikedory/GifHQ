e34d4cdf5b995221e4c5cb33ace92084623f8c6e
Mike Dory <mike@dory.me>
2012-12-31 10:40:50 +0530

    adding views for gifservers and gifs on their own

--- gifserver/views.py
+++ gifserver/views.py
@@ -1,15 +1,39 @@
-# Create your views here.
-from django.shortcuts import render_to_response
+# Views, son.
+from django.shortcuts import render_to_response, get_object_or_404
 from gifserver.models import Gifsite, Gif
 
 
+# home page
 def home(request):
     gif_sites = Gifsite.objects.all().order_by('-date_added')[:5]
-    gifs = Gif.objects.all().order_by('-date_added')[:5]
+    gifs = Gif.objects.all().order_by('-date_added')[:10]
+
     return render_to_response(
-        'gifserver/home.html',
-        {
+        'gifserver/home.html', {
             'gifsites': gif_sites,
             'gifs': gifs
+        }
+    )
+
+
+# a specific gifsite
+def gifsite(request, gifsite_name):
+    gif_site = get_object_or_404(Gifsite, name=gifsite_name)
+    gifs = Gif.objects.filter(gifsite=gif_site.id).order_by('-date_added')[:10]
+
+    return render_to_response(
+        'gifserver/gifsite.html', {
+            'gifsite': gif_site,
+            'gifs': gifs
+        }
+    )
+
+
+# a specific gif
+def gif(request, gif_id):
+    gif = get_object_or_404(Gif, pk=gif_id)
+    return render_to_response(
+        'gifserver/gif.html', {
+        'gif': gif
         }
     )
