15ec7c4e3619d764aa85c377c6b40694212ff33a
Mike Dory <mdory@kbsp.com>
2013-03-24 10:56:25 -0400

    adding a variable for the pool size (for concurrency)

--- gifhq/local_settings.sample.py
+++ gifhq/local_settings.sample.py
@@ -10,3 +10,6 @@ env.key_filename = '/path/to/.ssh/id_rsa'
 code_dir_root = '/path/to/code/directory'
 code_dir_target = '/path/to/code/directory/target'
 code_repo = 'git@gitrepo.com:user/REPO.git'
+
+# how many hosts you want to handle concurrently
+pool_size = 5
